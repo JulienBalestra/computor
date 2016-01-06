8b653ec3cff5ca0f9a276b892655009b7406dd88
Julien Balestra <julien.balestra@gmail.com>
2016-01-06 09:03:16 +0100

    Remove the checking ret function

--- sysadmin/install.sh
+++ sysadmin/install.sh
@@ -4,22 +4,21 @@ set -x
 COMPUTOR="https://github.com/JulienBalestra/computor.git"
 WORK_DIR="/usr/local/src"
 
-function check_ret
-{
-    if [ $1 -ne 0 ]
-    then
-        echo "fail [$2]"
-        exit 2
-    fi
-}
-
 function packages
 {
     apt-get update -qq
-    apt-get install python-pip python-dev
-    check_ret $? "apt-get install python-pip python-dev"
+    apt-get install git build-essential python-pip python-dev
+    if [ $1 -ne 0 ]
+    then
+        echo "fail [apt-get install python-pip python-dev]"
+        exit 2
+    fi
     pip install gunicorn
-    check_ret $? "pip install gunicorn flask"
+    if [ $1 -ne 0 ]
+    then
+        echo "fail [pip install gunicorn flask]"
+        exit 2
+    fi
 }
 
 function goto_cwd
@@ -31,8 +30,17 @@ function goto_cwd
 function clone_project
 {
     git clone ${COMPUTOR}
-    check_ret $? "git clone ${COMPUTOR}"
+    if [ $1 -ne 0 ]
+    then
+        echo "fail [git clone ${COMPUTOR}]"
+        exit 3
+    fi
     pip install -r computor/requirements.txt
+    if [ $1 -ne 0 ]
+    then
+        echo "fail [pip install -r computor/requirements.txt]"
+        exit 3
+    fi
 }
 
 packages
