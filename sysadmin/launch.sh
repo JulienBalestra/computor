804b111834c81885710e508893c2cd6cf671677a
Julien Balestra <julien.balestra@gmail.com>
2016-01-07 21:44:32 +0100

    Gunicorn log

--- sysadmin/launch.sh
+++ sysadmin/launch.sh
@@ -6,5 +6,6 @@ PYTHONPATH="/usr/local/src/computor"
 CHDIR="/usr/local/src/computor/srcs"
 APP="web_engine:application"
 BIND="0.0.0.0:80"
+LOG="/var/log/computor.log"
 
-/usr/local/bin/gunicorn ${APP} --chdir=${CHDIR} --pythonpath=${PYTHONPATH} --bind ${BIND} --daemon
+/usr/local/bin/gunicorn ${APP} --chdir ${CHDIR} --pythonpath ${PYTHONPATH} --bind ${BIND} --log-file ${LOG} --daemon
