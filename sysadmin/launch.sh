882fe94ee0251ece5a2529fd625aea49c82640d1
Julien Balestra <julien.balestra@gmail.com>
2016-01-08 08:27:09 +0100

    Gunicorn log

--- sysadmin/launch.sh
+++ sysadmin/launch.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Need sudo : bind over 0.0.0.0:80
+# Need sudo
 
 PYTHONPATH="/usr/local/src/computor"
 CHDIR="/usr/local/src/computor/srcs"
 APP="web_engine:application"
 BIND="0.0.0.0:80"
 LOG="/var/log/computor.log"
+LOG_LEVEL="debug"
 
-/usr/local/bin/gunicorn ${APP} --chdir ${CHDIR} --pythonpath ${PYTHONPATH} --bind ${BIND} --log-file ${LOG} --daemon
+/usr/local/bin/gunicorn ${APP} --chdir ${CHDIR} --pythonpath ${PYTHONPATH} --bind ${BIND} --log-file ${LOG} --log-level ${LOG_LEVEL} --daemon
